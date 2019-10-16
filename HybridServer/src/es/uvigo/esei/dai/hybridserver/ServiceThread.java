ab7915f7ec7ef03f73170c9d9d2335efa02c7cb6
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2019-10-16 14:22:57 +0200

    Cambios pequeños

--- HybridServer/src/es/uvigo/esei/dai/hybridserver/ServiceThread.java
+++ HybridServer/src/es/uvigo/esei/dai/hybridserver/ServiceThread.java
@@ -1,10 +1,14 @@
 package es.uvigo.esei.dai.hybridserver;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.io.Writer;
 import java.net.Socket;
 
+import es.uvigo.esei.dai.hybridserver.http.HTTPParseException;
+import es.uvigo.esei.dai.hybridserver.http.HTTPRequest;
 import es.uvigo.esei.dai.hybridserver.http.HTTPResponse;
 import es.uvigo.esei.dai.hybridserver.http.HTTPResponseStatus;
 
@@ -24,8 +28,19 @@ public class ServiceThread implements Runnable {
 		response.setVersion("HTTP/1.1");
 		response.setContent("Hybrid Server");
 
+		Reader reader;
+		try {
+			reader = new InputStreamReader(socket.getInputStream());
+			HTTPRequest request = new HTTPRequest(reader);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (HTTPParseException e) {
+			e.printStackTrace();
+		}
+		
 		Writer writer;
 		try {
+
 			writer = new OutputStreamWriter(socket.getOutputStream());
 			response.print(writer);
 			writer.close();
