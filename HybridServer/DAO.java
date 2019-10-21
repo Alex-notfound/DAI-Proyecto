2828c203858897e90b1637fc67efeb7a88ba6774
javivaleiras <56277439+javivaleiras@users.noreply.github.com>
2019-10-21 16:58:33 +0200

    correcion

--- HybridServer/DAO.java
+++
@@ -1,8 +0,0 @@
-package es.uvigo.esei.dai.hybridserver;
-
-public interface DAO {
-
-	public String get(String uuid);
-	public void delete(String uuid);
-	public String add(String content); //uuid aleatorio
-}
--- HybridServer/src/es/uvigo/esei/dai/hybridserver/DAO.java
+++ HybridServer/src/es/uvigo/esei/dai/hybridserver/DAO.java
@@ -3,5 +3,6 @@ package es.uvigo.esei.dai.hybridserver;
 public interface DAO {
 
 	public String get(String uuid);
-
+	public void delete(String uuid);
+	public String add(String content); //uuid aleatorio
 }
