c46ef67fa8bb5f4bee47517ae906b6e9e3511837
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2019-11-19 19:16:24 +0100

    Update html.mysql.sql

--- HybridServer/sql/html.mysql.sql
+++ HybridServer/sql/html.mysql.sql
@@ -3,28 +3,28 @@ USE `hstestdb`;
 
 DROP TABLE IF EXISTS `HTML`;
 CREATE  TABLE `hstestdb`.`HTML` (
-  `uuid` VARCHAR(36) NOT NULL ,
+  `uuid` CHAR(36) NOT NULL ,
   `content` TEXT NOT NULL ,
   PRIMARY KEY (`uuid`)
 );
 
 DROP TABLE IF EXISTS `XSD`;
 CREATE  TABLE `hstestdb`.`XSD` (
-  `uuid` VARCHAR(36) NOT NULL ,
+  `uuid` CHAR(36) NOT NULL ,
   `content` TEXT NOT NULL ,
   PRIMARY KEY (`uuid`)
 );
 
 DROP TABLE IF EXISTS `XML`;
 CREATE  TABLE `hstestdb`.`XML` (
-  `uuid` VARCHAR(36) NOT NULL ,
+  `uuid` CHAR(36) NOT NULL ,
   `content` TEXT NOT NULL ,
   PRIMARY KEY (`uuid`)
 );
 
 DROP TABLE IF EXISTS `XSLT`;
 CREATE  TABLE `hstestdb`.`XSLT` (
-  `uuid` VARCHAR(36) NOT NULL ,
+  `uuid` CHAR(36) NOT NULL ,
   `content` TEXT NOT NULL ,
   `xsd` CHAR(36) NOT NULL ,
   PRIMARY KEY (`uuid`)
