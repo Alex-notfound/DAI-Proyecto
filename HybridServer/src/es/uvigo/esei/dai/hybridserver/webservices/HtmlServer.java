49ddaf25f41ccd8063d2b32bd6d033e6fe450a6e
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2020-01-14 00:54:31 +0100

    Delete HtmlServer.java

--- HybridServer/src/es/uvigo/esei/dai/hybridserver/webservices/HtmlServer.java
+++
@@ -1,9 +0,0 @@
-package es.uvigo.esei.dai.hybridserver.webservices;
-
-import javax.xml.ws.Endpoint;
-
-public class HtmlServer {
-	public static void main(String[] args) {
-		Endpoint.publish("http://localhost:9876/calculus", new HtmlPageService());
-	}
-}
